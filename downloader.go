bda06e88fedb6aa6ece04d887c7f1212e07a5efc
Marc Santiago <marcanthonysanti@gmail.com>
2016-01-02 20:25:46 -0500

    concurrency added

--- downloader.go
+++ downloader.go
@@ -9,8 +9,8 @@ import (
 	"regexp"
 	"runtime"
 	"strings"
-	_ "sync"
-	"time"
+	"sync"
+	_ "time"
 )
 
 type arrayFlags []string
@@ -38,11 +38,11 @@ var videoFolder = "video_downloads"
 var mp3Folder = "mp3_files"
 var youtubeFolder = "youtube-dl-master"
 
-//var wg sync.WaitGroup
+var wg sync.WaitGroup
 
 func main() {
 
-	//runtime.GOMAXPROCS(MaxParallelism())
+	runtime.GOMAXPROCS(MaxParallelism())
 
 	var fileMode = flag.Bool("f", false, "If file mode is set to true then it will look for youtube urls serperated by a new line in the files path")
 	flag.Var(&urlStrings, "u", "Enter Youtube video url, each url needs the -u command before it")
@@ -55,16 +55,14 @@ func main() {
 				//TODO
 			} else {
 				if checkUrl(url) {
-					//wg.Add(1)
-					//go macDownloader(url)
-					//macDownloader(url)
-					downloadVideo(url, true)
+					wg.Add(1)
+					go downloadVideo(url, true)
 				} else {
 					fmt.Printf("The url %s is not a proper youtube url\n", url)
 				}
 			}
 		}
-		//wg.Wait()
+		wg.Wait()
 		fmt.Printf("Done converting videos\n")
 	} else {
 		//Load URLS from text file
@@ -118,107 +116,107 @@ func MaxParallelism() int {
 	return numCPU
 }
 
-func macDownloader(url string) {
-	path, err := filepath.Abs("")
-	if err != nil {
-		fmt.Println("Error locating absulte file paths")
-		os.Exit(1)
-	}
-
-	videoDirectoryPath := filepath.Join(path, videoFolder)
-	exist, err := folderExists(videoDirectoryPath)
-	if err != nil {
-		fmt.Println("The folder: %s either does not exist or is not in the same directory as make.go", videoDirectoryPath)
-		os.Exit(1)
-	}
-	if !exist {
-		os.Mkdir(videoDirectoryPath, 0777)
-	}
-
-	mp3DirectoryPath := filepath.Join(path, mp3Folder)
-	exist, err = folderExists(mp3DirectoryPath)
-	if err != nil {
-		fmt.Println("The folder: %s either does not exist or is not in the same directory as make.go", mp3DirectoryPath)
-		os.Exit(1)
-	}
-	if !exist {
-		os.Mkdir(mp3DirectoryPath, 0777)
-	}
-
-	youtubeDirectoryPath := filepath.Join(path, youtubeFolder)
-
-	// change the directory to the directory of the videodown
-	os.Chdir(youtubeDirectoryPath)
-	fmt.Printf("Downloading video %s\n", url)
-	cmd := exec.Command("/bin/sh", "-c", "python -m youtube_dl "+url)
-	cmd.Run()
-	fmt.Printf("Downloading video %s complete\n", url)
-	videos := checkExt(".mp4")
-	oldVideoPath := filepath.Join(youtubeDirectoryPath, videos[0])
-	newVideoPath := filepath.Join(videoDirectoryPath, videos[0])
-
-	// move the file the the vidoes directory
-	err = os.Rename(oldVideoPath, newVideoPath)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	// string magic to ensure the paths are correct, formats, and paths
-	// this could probably be cleaned up a bit
-	newVideoFileName := strings.Replace(oldVideoPath, videoDirectoryPath, "", -1)
-	newVideoFileName = strings.Replace(newVideoFileName, ".mp4", ".mp3", -1)
-	oldVideoPath = newVideoPath
-	oldVideoPath = strings.Replace(oldVideoPath, oldVideoPath, "\""+oldVideoPath+"\"", -1)
-	newVideoPath = filepath.Join(path, mp3Folder)
-	newVideoPath = filepath.Join(newVideoPath, newVideoFileName)
-	newVideoPath = strings.Replace(newVideoFileName, "youtube-dl-master", "mp3_files", -1)
-	newVideoPath = strings.Replace(newVideoPath, newVideoPath, "\""+newVideoPath+"\"", -1)
-
-	//make sure the file in the directory before executing the command
-	fmt.Printf("confirming path for %s\n", url)
-	os.Chdir(videoDirectoryPath)
-	stop := 0
-	exit := false
-	for {
-		videos = checkExt(".mp4")
-		if len(videos) > 0 {
-			for _, vidName := range videos {
-				if strings.Contains(strings.Replace(newVideoFileName, ".mp3", ".mp4", -1), vidName) == true {
-					exit = true
-					break
-				} else {
-					fmt.Printf("Video not found\n")
-					time.Sleep(1000 * time.Millisecond)
-					stop++
-				}
-			}
-		} else {
-			fmt.Printf("Nothing in folder\n")
-			time.Sleep(1000 * time.Millisecond)
-			stop++
-		}
-		if stop > 15 || exit {
-			break
-		}
-	}
-	fmt.Printf("Paths confirmed\n")
-
-	//Ensure path is located where the binaries live
-	os.Chdir(path)
-	fmt.Printf("Converting video to mp3\n")
-	ffmpegCommand := fmt.Sprintf("./ffmpeg -i %s %s", oldVideoPath, newVideoPath)
-	out, err := exec.Command("/bin/sh", "-c", ffmpegCommand).CombinedOutput()
-	if err != nil {
-		fmt.Printf("Error: %v %v\n", err, string(out))
-	} else {
-		fmt.Printf("Removing video\n")
-		err = os.Remove(strings.Replace(oldVideoPath, "\"", "", -1))
-		if err != nil {
-			fmt.Printf("Error: %v\n", err)
-		}
-	}
-	//wg.Done()
-}
+// func macDownloader(url string) {
+//   path, err := filepath.Abs("")
+//   if err != nil {
+//     fmt.Println("Error locating absulte file paths")
+//     os.Exit(1)
+//   }
+//
+//   videoDirectoryPath := filepath.Join(path, videoFolder)
+//   exist, err := folderExists(videoDirectoryPath)
+//   if err != nil {
+//     fmt.Println("The folder: %s either does not exist or is not in the same directory as make.go", videoDirectoryPath)
+//     os.Exit(1)
+//   }
+//   if !exist {
+//     os.Mkdir(videoDirectoryPath, 0777)
+//   }
+//
+//   mp3DirectoryPath := filepath.Join(path, mp3Folder)
+//   exist, err = folderExists(mp3DirectoryPath)
+//   if err != nil {
+//     fmt.Println("The folder: %s either does not exist or is not in the same directory as make.go", mp3DirectoryPath)
+//     os.Exit(1)
+//   }
+//   if !exist {
+//     os.Mkdir(mp3DirectoryPath, 0777)
+//   }
+//
+//   youtubeDirectoryPath := filepath.Join(path, youtubeFolder)
+//
+//   // change the directory to the directory of the videodown
+//   os.Chdir(youtubeDirectoryPath)
+//   fmt.Printf("Downloading video %s\n", url)
+//   cmd := exec.Command("/bin/sh", "-c", "python -m youtube_dl "+url)
+//   cmd.Run()
+//   fmt.Printf("Downloading video %s complete\n", url)
+//   videos := checkExt(".mp4")
+//   oldVideoPath := filepath.Join(youtubeDirectoryPath, videos[0])
+//   newVideoPath := filepath.Join(videoDirectoryPath, videos[0])
+//
+//   // move the file the the vidoes directory
+//   err = os.Rename(oldVideoPath, newVideoPath)
+//   if err != nil {
+//     fmt.Println(err)
+//   }
+//
+//   // string magic to ensure the paths are correct, formats, and paths
+//   // this could probably be cleaned up a bit
+//   newVideoFileName := strings.Replace(oldVideoPath, videoDirectoryPath, "", -1)
+//   newVideoFileName = strings.Replace(newVideoFileName, ".mp4", ".mp3", -1)
+//   oldVideoPath = newVideoPath
+//   oldVideoPath = strings.Replace(oldVideoPath, oldVideoPath, "\""+oldVideoPath+"\"", -1)
+//   newVideoPath = filepath.Join(path, mp3Folder)
+//   newVideoPath = filepath.Join(newVideoPath, newVideoFileName)
+//   newVideoPath = strings.Replace(newVideoFileName, "youtube-dl-master", "mp3_files", -1)
+//   newVideoPath = strings.Replace(newVideoPath, newVideoPath, "\""+newVideoPath+"\"", -1)
+//
+//   //make sure the file in the directory before executing the command
+//   fmt.Printf("confirming path for %s\n", url)
+//   os.Chdir(videoDirectoryPath)
+//   stop := 0
+//   exit := false
+//   for {
+//     videos = checkExt(".mp4")
+//     if len(videos) > 0 {
+//       for _, vidName := range videos {
+//         if strings.Contains(strings.Replace(newVideoFileName, ".mp3", ".mp4", -1), vidName) == true {
+//           exit = true
+//           break
+//         } else {
+//           fmt.Printf("Video not found\n")
+//           time.Sleep(1000 * time.Millisecond)
+//           stop++
+//         }
+//       }
+//     } else {
+//       fmt.Printf("Nothing in folder\n")
+//       time.Sleep(1000 * time.Millisecond)
+//       stop++
+//     }
+//     if stop > 15 || exit {
+//       break
+//     }
+//   }
+//   fmt.Printf("Paths confirmed\n")
+//
+//   //Ensure path is located where the binaries live
+//   os.Chdir(path)
+//   fmt.Printf("Converting video to mp3\n")
+//   ffmpegCommand := fmt.Sprintf("./ffmpeg -i %s %s", oldVideoPath, newVideoPath)
+//   out, err := exec.Command("/bin/sh", "-c", ffmpegCommand).CombinedOutput()
+//   if err != nil {
+//     fmt.Printf("Error: %v %v\n", err, string(out))
+//   } else {
+//     fmt.Printf("Removing video\n")
+//     err = os.Remove(strings.Replace(oldVideoPath, "\"", "", -1))
+//     if err != nil {
+//       fmt.Printf("Error: %v\n", err)
+//     }
+//   }
+//   //wg.Done()
+// }
 
 func downloadVideo(url string, mac bool) {
 	if mac == true {
@@ -255,20 +253,27 @@ func downloadVideo(url string, mac bool) {
 
 		fmt.Printf("Downloading video %s\n", url)
 
-		cmd := exec.Command("/bin/sh", "-c", "python -m  youtube-dl --extract-audio --audio-format mp3 -o \"%(title)s.%(ext)s \" "+url)
-		cmd.Run()
-		fmt.Printf("Downloading video %s complete\n", url)
-		// videos := checkExt(".mp4")
-		//     oldVideoPath := filepath.Join(youtubeDirectoryPath, videos[0])
-		//     newVideoPath := filepath.Join(videoDirectoryPath, videos[0])
-		//
-		//     // move the file the the vidoes directory
-		//     err = os.Rename(oldVideoPath, newVideoPath)
-		//     if err != nil {
-		//       fmt.Println(err)
-		//     }
+		out, err := exec.Command("/bin/sh", "-c", "python -m  youtube_dl --extract-audio --audio-format mp3 -o \"%(title)s.%(ext)s \" "+url).CombinedOutput()
+		if err != nil {
+			fmt.Printf("Error: %v %v\n", err, string(out))
+		} else {
+			fmt.Printf("Downloading video %s complete\n", url)
+		}
+
+		videos := checkExt(".mp3")
+		var oldVideoPath string
+		var newVideoPath string
+		for _, vid := range videos {
+			oldVideoPath = filepath.Join(youtubeDirectoryPath, vid)
+			newVideoPath = filepath.Join(mp3DirectoryPath, vid)
+			// move the file the the vidoes directory
+			os.Rename(oldVideoPath, newVideoPath)
+
+		}
+
 	} else {
 		//WINDOWS
 		//TODO
 	}
+	wg.Done()
 }
