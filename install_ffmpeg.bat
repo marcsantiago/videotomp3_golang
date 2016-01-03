e898d96a4d40a5fa7caf0c4d45f61386cc264b39
Marc Santiago <marcanthonysanti@gmail.com>
2016-01-03 11:05:17 -0500

    addind windows batch

---
+++ install_ffmpeg.bat
@@ -0,0 +1,6 @@
+@echo on
+
+MKDIR c:\FFMPEG
+XCOPY windows_ffmpeg c:\FFMPEG /y /s
+
+SETX PATH "%PATH%;c:\FFMPEG\bin" 
--- setup.go
+++ setup.go
@@ -16,10 +16,17 @@ func main() {
 	}
 
 	shellScript := filepath.Join(path, "install_ffmpeg.sh")
+	batScript := filepath.Join(path, "install_ffmpeg.bat")
 
 	if runtime.GOOS == "windows" {
-		//WINDOWS ENVIRONMENT CHECK, TO MAKE SURE THE BINARIES THAT WE ARE USING ARE THE CORRECT ONES
-		//TODO
+		fmt.Printf("Copying windows_ffmpeg contents to c:\\FFMPEG and addinf the path env c:\\FFMPEG\\bin")
+		out, err := exec.Command("cmd", "/C", batScript).CombinedOutput()
+		if err != nil {
+			fmt.Printf("Error: %v %v\n", err, string(out))
+		} else {
+			fmt.Printf("Homebrew and ffmpeg installed")
+		}
+
 	} else {
 		fmt.Printf("Please be patient installing homebrew, ffpmeg, and updating take a while")
 		out, err := exec.Command("/bin/sh", shellScript).CombinedOutput()
