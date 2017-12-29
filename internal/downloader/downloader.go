769e62933fefd6ab48705481210d8c9ca669d922
Marc Santiago <marcanthonysanti@gmail.com>
2017-12-29 18:26:32 -0500

    Downloader: Flipping logic

--- internal/downloader/downloader.go
+++ internal/downloader/downloader.go
@@ -38,27 +38,28 @@ func (d *Downloader) checkURL(URL string) bool {
 }
 
 func (d *Downloader) Run(URL string, video bool) {
+	defer d.wg.Done()
 	if !d.checkURL(URL) {
 		log.Fatal(logKey, "Url entered was not valid", "url", URL)
 	}
 
-	defer d.wg.Done()
-	if !video {
-		cmd := exec.Command("/usr/local/bin/youtube-dl", "--ignore-errors", "--extract-audio", "--audio-format", "mp3", "-o", "%(title)s.%(ext)s", URL)
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		err := cmd.Run()
-		if err != nil {
-			log.Fatal(logKey, "music cmd", "error", err)
-		}
-	} else {
+	if video {
 		cmd := exec.Command("/usr/local/bin/youtube-dl", "--ignore-errors", "-f", "bestvideo", URL)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 		err := cmd.Run()
 		if err != nil {
-			log.Fatal(logKey, "video cmd", "error", err)
+			log.Fatal(logKey, "Video cmd", "error", err)
 		}
+		return
+	}
+
+	cmd := exec.Command("/usr/local/bin/youtube-dl", "--ignore-errors", "--extract-audio", "--audio-format", "mp3", "-o", "%(title)s.%(ext)s", URL)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(logKey, "Music cmd", "error", err)
 	}
 	return
 }
